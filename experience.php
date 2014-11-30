bf22e150bebfc64626935e99e351e2101dac5a55
nadalsol <nadalsoler@gmail.com>
2014-11-30 18:39:46 +0100

    Fixed Sass media queries

--- experience.php
+++ experience.php
@@ -3,122 +3,120 @@ include_once ($_SERVER['DOCUMENT_ROOT'].'/inc/head.php');
 ?>
 
 <div id="content">
-  <div class="container">
 
-    <header>
-      <h1>Experience</h1>
-      <p>Designing since 1999</p>
+  <header>
+    <h1>Experience</h1>
+    <p>Designing since 1999</p>
+  </header>
+
+  <article>
+    <header class="heading-comp-toggle">
+      <h2>BeRepublic</h2>
+      <h3>Junio 2012 - 2014</h3>
+      <span class="icon icon-arrow-down"></span>
     </header>
+    <ul>
+      <li>Dirección creativa</li>
+      <li>Project management</li>
+      <li>Team leader</li>
+      <li>Dirección de arte</li>
+      <li>Arquitectura web</li>
+    </ul>
+    <p>Creación del departamento creativo de BeRepublic. Dirección creativa, dirección de arte, arquitectura de la información e interacción. Desarrollo de conceptos creativos para campañas digitales, webs, aplicaciones, social media, audiovisuales e imagen corporativa.</p>
+    <p>Marcas como Pepe Jeans, Munich, Jack Daniels, Barceló Hoteles, Amoma Hotels, Rastreator, Ajuntament de Barcelona e ICEMD.</p>
+    <p>Dirección de arte desde su creación de 3 start-ups: FancyBox, ChicPlace y Wallapop.</p>
+  </article>
 
-    <article>
-      <header class="heading-comp-toggle">
-        <h2>BeRepublic</h2>
-        <h3>Junio 2012 - 2014</h3>
-        <span class="icon icon-arrow-down"></span>
-      </header>
-      <ul>
-        <li>Dirección creativa</li>
-        <li>Project management</li>
-        <li>Team leader</li>
-        <li>Dirección de arte</li>
-        <li>Arquitectura web</li>
-      </ul>
-      <p>Creación del departamento creativo de BeRepublic. Dirección creativa, dirección de arte, arquitectura de la información e interacción. Desarrollo de conceptos creativos para campañas digitales, webs, aplicaciones, social media, audiovisuales e imagen corporativa.</p>
-      <p>Marcas como Pepe Jeans, Munich, Jack Daniels, Barceló Hoteles, Amoma Hotels, Rastreator, Ajuntament de Barcelona e ICEMD.</p>
-      <p>Dirección de arte desde su creación de 3 start-ups: FancyBox, ChicPlace y Wallapop.</p>
-    </article>
+  <article>
+    <header class="heading-comp-toggle">
+      <h2>Proximity</h2>
+      <h3>Mayo 2009 - Septiembre 2012</h3>
+      <span class="icon icon-arrow-down"></span>
+    </header>
+    <ul>
+      <li>???</li>
+    </ul>
+    <p>Concepto de campañas publicitarias, dirección de arte de sites, aplicaciones, emails, piezas para redes sociales, sesiones fotográficas y audiovisuales. Arquitectura de la información, planteamiento de wireframes. Trato directo con cliente y proveedores externos.</p>
+    <p>Marcas como Audi, Volkswagen, Skoda, Danone, Gas Natural, Pronovias, Eristoff, Bombay Sapphire, Torres (Viña Esmeralda).</p>
+  </article>
 
-    <article>
-      <header class="heading-comp-toggle">
-        <h2>Proximity</h2>
-        <h3>Mayo 2009 - Septiembre 2012</h3>
-        <span class="icon icon-arrow-down"></span>
-      </header>
-      <ul>
-        <li>???</li>
-      </ul>
-      <p>Concepto de campañas publicitarias, dirección de arte de sites, aplicaciones, emails, piezas para redes sociales, sesiones fotográficas y audiovisuales. Arquitectura de la información, planteamiento de wireframes. Trato directo con cliente y proveedores externos.</p>
-      <p>Marcas como Audi, Volkswagen, Skoda, Danone, Gas Natural, Pronovias, Eristoff, Bombay Sapphire, Torres (Viña Esmeralda).</p>
-    </article>
+  <article>
+    <header class="heading-comp-toggle">
+      <h2>DoubleYou</h2>
+      <h3>Enero 2008 - Marzo 2009</h3>
+      <span class="icon icon-arrow-down"></span>
+    </header>
+    <ul>
+      <li>???</li>
+    </ul>
+    <p>Dirección de arte de sites, emails, banners y sesiones fotográficas. Arquitectura de la información, planteamiento de wireframes.</p>
+    <p>Marcas como Cacique, Nike, Audi, GasNatural, PortAventura, San Miguel, Turismo España y Atrápalo.</p>
+  </article>
 
-    <article>
-      <header class="heading-comp-toggle">
-        <h2>DoubleYou</h2>
-        <h3>Enero 2008 - Marzo 2009</h3>
-        <span class="icon icon-arrow-down"></span>
-      </header>
-      <ul>
-        <li>???</li>
-      </ul>
-      <p>Dirección de arte de sites, emails, banners y sesiones fotográficas. Arquitectura de la información, planteamiento de wireframes.</p>
-      <p>Marcas como Cacique, Nike, Audi, GasNatural, PortAventura, San Miguel, Turismo España y Atrápalo.</p>
-    </article>
+  <article>
+    <header class="heading-comp-toggle">
+      <h2>Putput Comunicacions</h2>
+      <h3>Mayo 2007 - Octubre 2007</h3>
+      <span class="icon icon-arrow-down"></span>
+    </header>
+    <ul>
+      <li>???</li>
+    </ul>
+    <p>Diseño y conceptualización de proyectos digitales como la aplicación CRM para la Fira de Barcelona, diseño de una comunidad de blogs para la UOC o un site corporativo para el Hotel Bremon.</p>
+  </article>
 
-    <article>
-      <header class="heading-comp-toggle">
-        <h2>Putput Comunicacions</h2>
-        <h3>Mayo 2007 - Octubre 2007</h3>
-        <span class="icon icon-arrow-down"></span>
-      </header>
-      <ul>
-        <li>???</li>
-      </ul>
-      <p>Diseño y conceptualización de proyectos digitales como la aplicación CRM para la Fira de Barcelona, diseño de una comunidad de blogs para la UOC o un site corporativo para el Hotel Bremon.</p>
-    </article>
+  <article>
+    <header class="heading-comp-toggle">
+      <h2>UserZoom &amp; Xperience Consulting</h2>
+      <h3>Abril 2006 - Abril 2007</h3>
+      <span class="icon icon-arrow-down"></span>
+    </header>
+    <ul>
+      <li>???</li>
+    </ul>
+    <p>Consultoría especializada en la experiencia de usuario y usabilidad en internet. Diseño de páginas web, aplicaciones, diseño corporativo de la empresa así como de su producto Userzoom (herramienta propia de testeo remoto de usabilidad web).</p>
+    <p>Marcas como Universidad Carlos III de Madrid, DoubleRoom (Solmeliá), Institut Cartogràfic de Catalunya (ICC), la web del Comú d’Andorra La Vella.</p>
+  </article>
 
-    <article>
-      <header class="heading-comp-toggle">
-        <h2>UserZoom &amp; Xperience Consulting</h2>
-        <h3>Abril 2006 - Abril 2007</h3>
-        <span class="icon icon-arrow-down"></span>
-      </header>
-      <ul>
-        <li>???</li>
-      </ul>
-      <p>Consultoría especializada en la experiencia de usuario y usabilidad en internet. Diseño de páginas web, aplicaciones, diseño corporativo de la empresa así como de su producto Userzoom (herramienta propia de testeo remoto de usabilidad web).</p>
-      <p>Marcas como Universidad Carlos III de Madrid, DoubleRoom (Solmeliá), Institut Cartogràfic de Catalunya (ICC), la web del Comú d’Andorra La Vella.</p>
-    </article>
+  <article>
+    <header class="heading-comp-toggle">
+      <h2>Freelance</h2>
+      <h3>Marzo 2005 - Mayo 2006</h3>
+      <span class="icon icon-arrow-down"></span>
+    </header>
+    <ul>
+      <li>???</li>
+    </ul>
+    <p>Colaboración con Multiplica desarrollando el site corporativo del grupo Eat Out (Pans &amp; Company, Bocatta, Fresh &amp; Ready, FresCo, Pastafiore&hellip;) y una propuesta de diseño para el site corporativo de Gallina Blanca.</p>
+    <p>Colaboración con la agencia ADN Comunicació en proyectos de comunicación impresa y proyectos de identidad corporativa.</p>
+    <p>Colaboración con las siguientes empresas en sus proyectos:</p>
+    <dl>
+      <dt>Interpartes</dt>
+      <dd>Imagen corporativa y papelería.</dd>
+      <dt>RSA Advocats</dt>
+      <dd>Imagen corporativa y papelería.</dd>
+      <dt>Gentebonita</dt>
+      <dd>Imagen corporativa, papelería y diferentes piezas de promoción.</dd>
+      <dt>L’estenedor de la Marta</dt>
+      <dd>Imagen corporativa, papelería, rótulo tienda, promoción y web.</dd>
+      <dt>Cardio Endurance</dt>
+      <dd>Imagen corporativa y papelería.</dd>
+    </dl>
+  </article>
 
-    <article>
-      <header class="heading-comp-toggle">
-        <h2>Freelance</h2>
-        <h3>Marzo 2005 - Mayo 2006</h3>
-        <span class="icon icon-arrow-down"></span>
-      </header>
-      <ul>
-        <li>???</li>
-      </ul>
-      <p>Colaboración con Multiplica desarrollando el site corporativo del grupo Eat Out (Pans &amp; Company, Bocatta, Fresh &amp; Ready, FresCo, Pastafiore&hellip;) y una propuesta de diseño para el site corporativo de Gallina Blanca.</p>
-      <p>Colaboración con la agencia ADN Comunicació en proyectos de comunicación impresa y proyectos de identidad corporativa.</p>
-      <p>Colaboración con las siguientes empresas en sus proyectos:</p>
-      <dl>
-        <dt>Interpartes</dt>
-        <dd>Imagen corporativa y papelería.</dd>
-        <dt>RSA Advocats</dt>
-        <dd>Imagen corporativa y papelería.</dd>
-        <dt>Gentebonita</dt>
-        <dd>Imagen corporativa, papelería y diferentes piezas de promoción.</dd>
-        <dt>L’estenedor de la Marta</dt>
-        <dd>Imagen corporativa, papelería, rótulo tienda, promoción y web.</dd>
-        <dt>Cardio Endurance</dt>
-        <dd>Imagen corporativa y papelería.</dd>
-      </dl>
-    </article>
-
-    <article>
-      <header class="heading-comp-toggle">
-        <h2>eMascaró</h2>
-        <h3>Junio 2002 - Marzo 2005</h3>
-        <span class="icon icon-arrow-down"></span>
-      </header>
-      <ul>
-        <li>???</li>
-      </ul>
-      <p>Responsable del departamento de diseño y dirección de arte de páginas web, emailings, elearning, identidad corporativa, comunicación impresa y diseño editorial.</p>
-      <p>Marcas como Hotel Majestic, el Institut de Cultura de Barcelona, Arvato Services, Universitat de Barcelona Virtual (UBV), Barcelona Activa, Patanè &amp; Partners.</p>
-    </article>
+  <article>
+    <header class="heading-comp-toggle">
+      <h2>eMascaró</h2>
+      <h3>Junio 2002 - Marzo 2005</h3>
+      <span class="icon icon-arrow-down"></span>
+    </header>
+    <ul>
+      <li>???</li>
+    </ul>
+    <p>Responsable del departamento de diseño y dirección de arte de páginas web, emailings, elearning, identidad corporativa, comunicación impresa y diseño editorial.</p>
+    <p>Marcas como Hotel Majestic, el Institut de Cultura de Barcelona, Arvato Services, Universitat de Barcelona Virtual (UBV), Barcelona Activa, Patanè &amp; Partners.</p>
+  </article>
   
-  </div>
 </div>
 
 <?php
