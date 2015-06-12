b701cb2ac18f9b135af862ec73ea735b074ece55
Nadal Soler <nadal.soler@redbooth.com>
2015-06-12 21:30:41 +0200

    Hide the WP Admin Bar when logged in

--- app397377180/wp-content/themes/bridge-child/functions.php
+++ app397377180/wp-content/themes/bridge-child/functions.php
@@ -1,9 +1,19 @@
 <?php
 
-// enqueue the child theme stylesheet
 
-Function wp_schools_enqueue_scripts() {
-wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
-wp_enqueue_style( 'childstyle' );
+/**
+ * Enqueue the child theme stylesheet
+ *
+ */
+function wp_schools_enqueue_scripts() {
+  wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
+  wp_enqueue_style( 'childstyle' );
 }
-add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
+add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11 );
+
+
+/**
+ * Hide the Admin Bar
+ *
+ */
+add_filter('show_admin_bar', '__return_false');
