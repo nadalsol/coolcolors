f2bcc0a299162fd230bf417b2637504baf46a38b
Nadal Soler <nadal.soler@redbooth.com>
2015-09-18 09:32:44 +0200

    Added 'lang' attribute to some texts

--- index.php
+++ index.php
@@ -61,8 +61,8 @@
     <div class="about-me">
       <div class="row">
         <div class="small-12 medium-8 medium-centered large-6 columns">
-          <h2 class="about-me__title text-uppercase">¡Hola!</h2>
-          <h3 class="about-me__subtitle">Me llamo Miriam, soy diseñadora visual, de experiencia de usuario y arquitecta de la información.</h3>
+          <h2 class="about-me__title text-uppercase" lang="es-ES">¡Hola!</h2>
+          <h3 class="about-me__subtitle" lang="es-ES">Me llamo Miriam, soy diseñadora visual, de experiencia de usuario y arquitecta de la información.</h3>
           <p>Ésta es una selección de mis trabajos, entra para ver el detalle.</p>
         </div>
       </div>
