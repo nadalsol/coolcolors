9a9f6ee01178ba2b622f7501db95cb37b530be8d
Nadal Soler <nadal.soler@redbooth.com>
2015-10-03 21:03:19 +0200

    Applied new styles to 'About me' panel

--- templates/about-me.php
+++ templates/about-me.php
@@ -1,10 +1,23 @@
 <!-- About me -->
 <div class="about-me">
+
   <div class="row">
-    <div class="small-12 medium-8 medium-centered large-6 columns">
-      <h2 class="about-me__title text-uppercase" lang="es-ES">¡Hola!</h2>
-      <h3 class="about-me__subtitle" lang="es-ES">Me llamo Miriam, soy diseñadora visual, de experiencia de usuario y arquitecta de la información.</h3>
-      <p>Ésta es una selección de mis trabajos, entra para ver el detalle.</p>
+    <div class="small-12 medium-10 medium-centered columns">
+      <div class="row">
+        <div class="small-12 medium-3 columns">
+          <h2 class="about-me__title text-uppercase">Miriam Soto</h2>
+          <h3 class="about-me__subtitle text-uppercase">Information Architect, <abbr title="User Experience">UX</abbr> &amp; Visual Designer.</h3>
+        </div>
+        <div class="small-12 medium-8 medium-offset-1 columns" lang="es-ES">
+          <h4 class="about-me__lead text-uppercase">Claro. Sencillo. Atractivo.</h4>
+          <div class="about-me__desc">
+            <p>Éstas son las palabras que describen un buen proyecto digital.</p>
+            <p>En mis proyectos siempre realizo una estructura clara de la información utilizando una gran dosis de sentido común, una navegación e interfaz que proporcionen una experiencia fluida y satisfactoria hacia nuestro objetivo  utilizando la empatía con el usuario y una imagen limpia y seductora que capte la atención al instante.</p>
+            <p>Todo nace en el papel en blanco, de una idea y de un objetivo.</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
+
 </div>
