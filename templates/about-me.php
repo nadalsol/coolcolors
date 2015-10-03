929815b56966c8dfd31d6b3817d4a66202363ad8
Nadal Soler <nadal.soler@redbooth.com>
2015-10-03 21:53:33 +0200

    Improved responsive styles

--- templates/about-me.php
+++ templates/about-me.php
@@ -2,7 +2,7 @@
 <div class="about-me">
 
   <div class="row">
-    <div class="small-12 medium-10 medium-centered columns">
+    <div class="small-12 medium-11 medium-centered large-10 columns">
       <div class="row">
         <div class="small-12 medium-3 columns">
           <h2 class="about-me__title text-uppercase">Miriam Soto</h2>
