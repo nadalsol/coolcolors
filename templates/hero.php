7b2ed630b53ba8ab76c674371c9769f22cbea5c7
Nadal Soler <nadal.soler@redbooth.com>
2015-10-03 17:20:07 +0200

    Positioned <h1> to the bottom of the Hero

--- templates/hero.php
+++ templates/hero.php
@@ -1,19 +1,15 @@
 <!-- Hero -->
 <div class="hero" id="hero">
 
+  <h1 class="hero__title text-uppercase">
+    <span><abbr title="User Experience">UX</abbr> &amp; Visual</span>
+    <span>Designer</span>
+  </h1>
+
   <video autoplay loop muted poster="img/hero/dancing.jpg">
     <source src="video/dancing.mp4" type="video/mp4">
     <source src="video/dancing.ogg" type="video/ogg">
     <source src="video/dancing.webm" type="video/webm">
   </video>
-
-  <div class="row">
-    <div class="small-12 columns">
-      <h1 class="hero__title text-uppercase">
-        <span><abbr title="User Experience">UX</abbr> &amp; Visual</span>
-        <span>Designer</span>
-      </h1>
-    </div>
-  </div>
   
 </div>
