82fcaf517fc3265bd1e1019d765976bb2c06bf7f
Nadal Soler <nadal.soler@redbooth.com>
2015-10-14 10:41:12 +0200

    Added grey color to About Project panel

--- inc/carousel.php
+++ inc/carousel.php
@@ -13,26 +13,30 @@
 
 <!-- Carousel -->
 <div class="carousel carousel--offset slide" id="carousel" data-ride="carousel">
+  <div class="row">
+    <div class="small-12 columns">
 
-  <ol class="carousel-indicators">
-    <?php for ($i=0; $i < count($images_array); $i++) : ?>
-    <li data-target="#carousel" data-slide-to="<?php echo $i; ?>" <?php if($i==0) {echo 'class="active"';} ?>></li>
-    <?php endfor; ?>
-  </ol>
+      <ol class="carousel-indicators">
+        <?php for ($i=0; $i < count($images_array); $i++) : ?>
+        <li data-target="#carousel" data-slide-to="<?php echo $i; ?>" <?php if($i==0) {echo 'class="active"';} ?>></li>
+        <?php endfor; ?>
+      </ol>
 
-  <div class="carousel-inner" role="listbox">
-    <?php for ($i=0; $i < count($images_array); $i++) : ?>
-    <div class="item <?php if($i==0) {echo 'active';} ?>">
-      <img src="/img<?php echo $projects_url; ?><?php echo $images_array[$i]; ?>" alt=" ">
+      <div class="carousel-inner" role="listbox">
+        <?php for ($i=0; $i < count($images_array); $i++) : ?>
+        <div class="item <?php if($i==0) {echo 'active';} ?>">
+          <img src="/img<?php echo $projects_url; ?><?php echo $images_array[$i]; ?>" alt=" ">
+        </div>
+        <?php endfor; ?>
+      </div>
+
+      <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+        <span class="btn btn--arrow-left">‹</span>
+      </a>
+      <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+        <span class="btn btn--arrow-right">›</span>
+      </a>
+
     </div>
-    <?php endfor; ?>
   </div>
-
-  <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-    <span class="btn btn--arrow-left">‹</span>
-  </a>
-  <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-    <span class="btn btn--arrow-right">›</span>
-  </a>
-
 </div>
