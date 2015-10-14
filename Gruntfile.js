0cda2ec9898ac05b625d7a722a96f8945c4d2230
Nadal Soler <nadal.soler@redbooth.com>
2015-10-14 10:22:29 +0200

    Added 'grunt-contrib-sass' and 'grunt-contrib-watch' to look for changes in Sass files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,21 @@
+'use strict';
 module.exports = function(grunt) {
 
   // 1. All configuration goes here 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    // 2. Configuration for concatinating, minifing... files goes here.
+    // 2. Configuration files goes here (concatinating, minifing...)
+    sass: {
+      dev: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'css/base.css': 'sass/base.scss'
+        }
+      }
+    },
     concat: {
       dist: {
         src: [
@@ -20,6 +31,16 @@ module.exports = function(grunt) {
         src: 'js/build/production.js',
         dest: 'js/build/production.min.js'
       }
+    },
+    watch: {
+      stylesheets: {
+        files: ['sass/**/*.{sass,scss,css}'],
+        tasks: ['sass:dev']
+      },
+      js: {
+        files: ['js/**/*.js'],
+        tasks: ['concat']
+      }
     }
 
   });
@@ -27,9 +48,10 @@ module.exports = function(grunt) {
   // 3. Where we tell Grunt we plan to use this plug-in.
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  // grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('default', ['concat', 'uglify', 'watch']);
 
 };
