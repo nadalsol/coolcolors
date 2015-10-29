8f8319713c72b50e5b8c2ed909751a8073229ce4
Nadal Soler <nadal.soler@redbooth.com>
2015-10-29 12:56:58 +0100

    Added tags to About Project

--- inc/about-project.php
+++ inc/about-project.php
@@ -4,6 +4,10 @@
     if ($projects->url == $_SERVER['REQUEST_URI']) {
       $projects_title = $projects->title;
       $projects_category = $projects->category;
+      $tags_array = array();
+      foreach($projects->tags->tag as $tag) {
+        array_push($tags_array, $tag);
+      }
       $projects_desc = $projects->desc;
     }
   endforeach;
@@ -17,7 +21,14 @@
       <div class="row">
         <div class="small-12 columns">
           <h2 class="about__title text-uppercase"><?php echo $projects_title; ?></h2>
-          <h3 class="about__subtitle text-uppercase"><?php echo $projects_category; ?></h3>
+          <h3 class="about__subtitle text-uppercase">
+          <?php
+            echo '<span class="about__subtitle__category">' . $projects_category . '</span>';
+            for ($i=0; $i < count($tags_array); $i++) {
+              echo '<span class="about__subtitle__tag">' . $tags_array[$i] . '</span>';
+            }
+          ?>
+          </h3>
         </div>
         <div class="small-12 columns" lang="es-ES">
           <div class="about__desc"><?php echo $projects_desc; ?></div>
