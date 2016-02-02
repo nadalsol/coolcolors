8ebd351dcebf91e97315fe65d58636dde28e43db
Nadal Soler <nadal.soler@redbooth.com>
2016-02-02 19:05:54 +0100

    Added category and title on rollover a project

--- inc/portfolio.php
+++ inc/portfolio.php
@@ -8,13 +8,19 @@
     ?>
       <li class="portfolio__project">
         <a href="<?php echo $projects->url; ?>">
-          <ul>
-          <?php
-            foreach($projects->tags->tag as $tag) {
-            echo '<li>' . $tag .'</li>';
-            }
-          ?>
-          </ul>
+          <div>
+            <h5>
+              <?php echo $projects->category; ?>
+              <span><?php echo $projects->title; ?></span>
+            </h5>
+            <ul>
+            <?php
+              foreach($projects->tags->tag as $tag) {
+              echo '<li>' . $tag .'</li>';
+              }
+            ?>
+            </ul>
+          </div>
           <img src="/img<?php echo $projects->url; ?><?php echo $projects->image_thumb; ?>" alt="<?php echo $projects->title; ?>">
         </a>
       </li>
@@ -22,9 +28,11 @@
     <?php for ($i=0; $i < 3; $i++) : ?>
       <li class="portfolio__project portfolio__project--soon">
         <a href="#">
-          <ul>
-            <li>Coming soon&hellip;</li>
-          </ul>
+          <div>
+            <ul>
+              <li>Coming soon&hellip;</li>
+            </ul>
+          </div>
           <span>
             <img src="/img/portfolio/rocket.svg" alt="Coming soon&hellip;">
           </span>
